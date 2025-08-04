f08cdad4770e28650e2e472b0b56c8fff32eea9d
mahesh kumar chhedal <mheskshedal11@gmail.com>
2025-08-04 17:15:13 +0545

    add and user and video model

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import dotenv from "dotenv";
-
-dotenv.config({
-  path: "./env",
-});
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
+
+dotenv.config({ path: "./.env" });
+
+const PORT = process.env.PORT || 8000;
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`server is running at port: ${process.env.PORT}`);
+    app.listen(PORT, () => {
+      console.log(`Server is running at port: ${PORT}`);
     });
   })
-  .catch((err) => console.log("mongo db connection failed !!!"));
+  .catch((err) => {
+    console.error("MongoDB connection failed:", err);
+  });
