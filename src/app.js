bb8d8de29cf7847d6e71c880429edb7f21924cb6
mahesh kumar chhedal <mheskshedal11@gmail.com>
2025-08-05 15:37:45 +0545

    add register controller

--- src/app.js
+++ src/app.js
@@ -10,15 +10,16 @@ app.use(
     credentials: true,
   })
 );
-//Routes import
-import userRouter from "./routes/user.router.js";
-
-//route declaration
-app.use("/api/v1/users", userRouter);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//Routes import
+import userRouter from "./routes/user.router.js";
+
+//route declaration
+app.use("/api/v1/users", userRouter);
 
 export { app };
